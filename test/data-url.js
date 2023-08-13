9fe3d5b4f72bb4c2fd24ae761413c533b6d7bb87
Romain Menke <romainmenke@gmail.com>
2023-08-13 17:16:25 +0200

    properly join conditions on imports that can not be inlined

--- test/data-url.js
+++ test/data-url.js
@@ -5,4 +5,6 @@ const test = require("ava")
 // internal tooling
 const checkFixture = require("./helpers/check-fixture")
 
-test("should inline data urls", checkFixture, "data-url")
+test("should inline data urls", checkFixture, "data-url", null, null, [
+  `Unable to import 'foo.css' from a stylesheet that is embedded in a data url`,
+])
