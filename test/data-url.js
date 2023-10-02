6be601c3adf8c51d74d40f9e765dc460ef9f54d0
Romain Menke <11521496+romainmenke@users.noreply.github.com>
2023-10-02 18:59:40 +0200

    Error on embedded imports in data urls (#543)
    * properly ignore embedded imports in data urls
    
    * add a warning
    
    * wording
    
    * wording
    
    * cleaner solution
    
    * hard error

--- test/data-url.js
+++ test/data-url.js
@@ -1,8 +1,27 @@
 "use strict"
 // external tooling
 const test = require("ava")
+const postcss = require("postcss")
+
+// plugin
+const atImport = require("..")
 
 // internal tooling
 const checkFixture = require("./helpers/check-fixture")
 
 test("should inline data urls", checkFixture, "data-url")
+
+test("should error on relative urls from stylesheets in data urls", t => {
+  return postcss()
+    .use(atImport())
+    .process(
+      "@import url(data:text/css;base64,QGltcG9ydCB1cmwoZm9vLmNzcyk7CgpwIHsKICBjb2xvcjogYmx1ZTsKfQo=);",
+      { from: undefined }
+    )
+    .catch(error =>
+      t.regex(
+        error.message,
+        /Unable to import '(?:.*?)' from a stylesheet that is embedded in a data url/
+      )
+    )
+})
