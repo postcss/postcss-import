7b15449444e209011ded6978674e31e6dc2fcc7d
Romain Menke <romainmenke@gmail.com>
2023-12-09 19:22:14 +0100

    increase test coverage and cleanup

---
+++ test/supports-condition.js
@@ -0,0 +1,12 @@
+"use strict"
+// external tooling
+const test = require("ava")
+
+// internal tooling
+const checkFixture = require("./helpers/check-fixture")
+
+test(
+  "should resolve supports conditions of import statements",
+  checkFixture,
+  "supports-condition-import"
+)
